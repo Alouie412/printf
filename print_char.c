9d7cb43eb31bc5d421efaf08b73e79fd18048680
nhlshstr <nhlshstr@gmail.com>
2019-10-28 11:17:54 -0700

    initial commit

---
+++ print_char.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *_printChar - function to 
+ *
+ * @args: list of arguments
+ *
+ * Return: Returns the input string length
+ */
+int _printChar(va_list args, int *inlength)
+{	
+	char a;
+	a = va_arg(args, int);
+
+	_putchar(a);
+	return(++inlength);
+}
