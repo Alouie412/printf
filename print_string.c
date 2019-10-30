7c9acca303478d59e63dfd49f355f49e1616b47f
Alouie412 <alouie412@gmail.com>
2019-10-30 17:12:30 +0000

    Pushing files

--- print_string.c
+++ print_string.c
@@ -68,7 +68,8 @@ int print_string(va_list arg, int *in_length, char ltr)
 		length++;
 
 	ptr = malloc(length * sizeof(char));
-
+	if (ptr == NULL)
+		return (-1);
 	for (i = 0; i < length; i++)
 		ptr[i] = string[i];
 
